b0c59fc486173f655bd1c19909e99f6cdd750fee
Shahi Shohanur Hamid <shahipapon@rocketmail.com>
2024-01-23 22:18:58 +0600

    add child route to check

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +29,14 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <nav>
+        <Link to="/React-Router-Seo/">Home</Link>
+        {" | "}
+        <Link to="/React-Router-Seo/users">users</Link>
+      </nav>
+
+      <Outlet />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -11,19 +11,8 @@ const router = createBrowserRouter([
     path: "/React-Router-Seo/",
     element: <App/>,
   
-    // children:[
-    //   {
-    //     path: "/React-Router-Seo/users",
-    //     element: <Users/>,
-    //   },
-    //   {
-    //     path: "React-Router-Seo/user/:userId",
-    //     element: <UserDetails />,
-    //   },
-    // ],
-    errorElement: <ErrorPage />,
-  },
-  {
+    children:[
+      {
     path: "/React-Router-Seo/users",
     element: <Users/>,
   },
@@ -31,6 +20,17 @@ const router = createBrowserRouter([
     path: "/React-Router-Seo/user/:userId",
     element: <UserDetails />,
   },
+    ],
+    errorElement: <ErrorPage />,
+  },
+  // {
+  //   path: "/React-Router-Seo/users",
+  //   element: <Users/>,
+  // },
+  // {
+  //   path: "/React-Router-Seo/user/:userId",
+  //   element: <UserDetails />,
+  // },
 
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
