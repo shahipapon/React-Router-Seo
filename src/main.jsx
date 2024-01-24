051418123ae21f355a2d307098795d05f887a03b
Shahi Shohanur Hamid <shahipapon@rocketmail.com>
2024-01-24 11:10:06 +0600

    update routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,43 +4,51 @@ import App from "./App.jsx";
 import Users from "./pages/users.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ErrorPage from './error-page.jsx';
-import UserDetails from './pages/userDetails.jsx';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/React-Router-Seo/",
-    element: <App/>,
-  
-  //   children:[
-  //     {
-  //   path: "/React-Router-Seo/users",
-  //   element: <Users/>,
-  // },
-  // {
-  //   path: "/React-Router-Seo/user/:userId",
-  //   element: <UserDetails />,
-  // },
-  //   ],
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/React-Router-Seo/users",
-    element: <Users/>,
-  },
-  {
-    path: "/React-Router-Seo/user/:userId",
-    element: <UserDetails />,
-  },
+import ErrorPage from "./error-page.jsx";
+import UserDetails from "./pages/userDetails.jsx";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App/>,
+
+//   //   children:[
+//   //     {
+// path: "/users",
+// element: <Users/>,
+//   // },
+//   // {
+//   //   path: "/user/:userId",
+//   //   element: <UserDetails />,
+//   // },
+//   //   ],
+//     errorElement: <ErrorPage />,
+//   },
+//   {
+//     path: "/users",
+//     element: <Users/>,
+//   },
+//   {
+// path: "/user/:userId",
+// element: <UserDetails />,
+//   },
+
+// ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <HelmetProvider>
-    <RouterProvider router={router} />
-
-     </HelmetProvider>
+    <HelmetProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/user/:userId" element={<UserDetails />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Router>
+    </HelmetProvider>
   </React.StrictMode>
 );
