3f0729dace31aff568b0733f042d967dc6ef1a28
Shahi Shohanur Hamid <shahipapon@rocketmail.com>
2024-01-24 23:17:01 +0600

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import DynamicMeta from '../components/DynamicMeta';
 
 const Home = () => {
   const [count, setCount] = useState(0);
   return (
     <>
-    {/* <DynamicMeta
+    <DynamicMeta
       title="How to Create Dynamic Meta Tags"
       description="Learn how to create dynamic meta tags in a React.js application for improved SEO."
       canonicalUrl="https://shahipapon.github.io/React-Router-Seo"
-    /> */}
+    />
     
     <h1>Vite + React</h1>
     <div className="card">
