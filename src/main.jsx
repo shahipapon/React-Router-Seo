63cf03cd899444868af9c64b252a79e22c5df81d
Shahi Shohanur Hamid <shahipapon@rocketmail.com>
2024-01-24 22:16:45 +0600

    update helmet to top

--- src/main.jsx
+++ src/main.jsx
@@ -38,9 +38,9 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <HelmetProvider>
+  <HelmetProvider>
+    <React.StrictMode>
       <RouterProvider router={router} />
-    </HelmetProvider>
-  </React.StrictMode>
+    </React.StrictMode>
+  </HelmetProvider>
 );
